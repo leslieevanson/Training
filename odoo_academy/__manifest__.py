c55ad61653a5cf461e829659492127019b065b96
leslieevanson <leslie@retailcaribbean.com>
2022-06-12 00:11:02 +0000

    Views exercise

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -25,7 +25,8 @@
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
-        'views/academy_menuitems.xml',       
+        'views/academy_menuitems.xml', 
+        'views/course_views.xml',
     ],
     
     'demo': [
